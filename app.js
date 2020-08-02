63836eeb23b387a8148c581d569fe6f6313390ba
Blackbird <ksilva.rod@gmail.com>
2020-08-01 19:55:03 -0500

    prompt-inquirer

--- app.js
+++ app.js
@@ -31,4 +31,50 @@ conn.connect(function (err) {
   function clearConsoleAndScrollbackBuffer() {
     process.stdout.write("\u001b[3J\u001b[2J\u001b[1J");console.clear();
   }
-   
+//Main function that collects inputs from user and shows them the menu and data
+function beginSearch() {
+
+    //Generates main menu
+    inquirer
+    .prompt({
+        type: "rawlist",
+        name: "optionList",
+        message: "Please select an option: ",
+        choices: [
+          "View all employees",
+          "View all departments",
+          "View all roles",
+          "Add employee",
+          "Add department",
+          "Add role",
+          "Update employee's role",
+          "Exit Application"
+        ]
+      })
+      .then(function(response) {
+        switch(response.optionList){
+        case "View all employees":
+          //calls function that shows all employees
+          viewAllEmployees();
+          break;
+        
+        case "View all departments":
+          //calls function that shows all departments
+          viewAllDepartments();
+          break;
+  
+        case "View all roles":
+          //calls function that shows all roles
+          viewAllRoles();
+          break;
+  
+        case "Add department":
+          //calls function that shows all departments
+          addDepartment();
+          break;
+        
+        case "Add employee":
+          //calls function to add a new employee
+          addEmployee();
+          break;
+     
