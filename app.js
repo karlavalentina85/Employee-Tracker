f2697085d4f966634ba71290d2c4c1e5a04bc4d0
Blackbird <ksilva.rod@gmail.com>
2020-08-01 19:42:26 -0500

    call packages

---
+++ app.js
@@ -0,0 +1,7 @@
+//Call packages needed to make this work
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const cTable = require('console.table');
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
