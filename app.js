9e6d54a001d613007ba7d42d1d87ece38adaacee
Blackbird <ksilva.rod@gmail.com>
2020-08-01 19:45:24 -0500

    mysql connection

--- app.js
+++ app.js
@@ -5,3 +5,30 @@ const cTable = require('console.table');
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
+
+//MySQL connection parameters
+var conn = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "cms"
+  });
+
+ //Connection to database
+conn.connect(function (err) {
+    if (err) throw err;
+  });
+  
+  function showApHeader () {
+    console.log(
+      chalk.yellow(
+        figlet.textSync('EmpTrac 1.0', { horizontalLayout: 'full' })
+      )
+    );  
+  }
+  
+  function clearConsoleAndScrollbackBuffer() {
+    process.stdout.write("\u001b[3J\u001b[2J\u001b[1J");console.clear();
+  }
+   
